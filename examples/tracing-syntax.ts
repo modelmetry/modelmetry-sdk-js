78b75721e41becf9410e1de1b99c58363be97c39
Lazhar <hello@lazharichir.com>
2024-08-04 20:00:18 +0100

    chore: add asyncAsleeps to exampel

--- examples/tracing-syntax.ts
+++ examples/tracing-syntax.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { env } from "node:process";
 import { ModelmetryClient } from "../src/sdk";
+import { asyncSleep } from "../src/utils/dates";
 
 export const example = async () => {
   const TENANT_ID = String(env.TENANT_ID); // e.g., ten_acmelimited12
@@ -17,18 +18,36 @@ export const example = async () => {
   const trace = obs.newTrace("root");
 
   const span1 = trace.newSpan("root.span1");
+  span1.newEvent("something happened in span1");
+
+  await asyncSleep(50);
+
   const span1_1 = span1.newSpan("root.span1.1");
+  await asyncSleep(50);
 
   span1_1.end();
   span1.end();
 
   const span2 = trace.newSpan("root.span2");
-  const span2_1 = span2.newSpan("root.span2.1");
-  const span2_2 = span2.newSpan("root.span2.2");
+  span2.newEvent("something happened in span2");
+  span2.newEvent("something happened again in span2");
+  await asyncSleep(50);
 
+  const span2_1 = span2.newSpan("root.span2.1");
+  span2_1.newEvent("something happened in span2.1");
+  await asyncSleep(50);
+
+  const span2_2 = span2.newSpan("root.span2.2");
+  span2_2.newEvent("something happened in span2.2");
+  span2_2.newEvent("something happened again in span2.2");
+  await asyncSleep(50);
   span2_1.end();
+
+  await asyncSleep(50);
   span2_2.end();
   span2.end();
+
+  await asyncSleep(50);
 
   trace.end();
 
